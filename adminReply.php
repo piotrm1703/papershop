1e9b67b3da2f1ec55061f1712304ff7443dbb780
piotrm1703 <pjotr.marcel@gmail.com>
2018-02-26 19:37:42 +0100

    mail
    asd

--- adminReply.php
+++ adminReply.php
@@ -13,15 +13,10 @@ if(isset($_POST['submit'])){
     foreach (($messagesArray) as $k=> $v) {
 
         if ($v['id'] === $currentPage) {
-            //    ini_set('SMTP', "smtp.gmail.com");
-            //    ini_set('smtp_port', "465");
-            //    ini_set('username', "papershoppingorder@gmail.com");
-            //    ini_set('password', "papershop2");
-            //    ini_set('sendmail_from', "papershoppingorder@gmail.com");
             $to = $v['email'];
             $subject = 'Odpowiedz na pytanie dotyczące: ' . $v['subject'];
-            $txt = htmlEscape($_POST('replymsg'));
-            $headers = "From: administracja@papershop.com" . "\r\n";
+            $txt = ($_POST['replymsg']);
+            $headers = "From: administracja@papershop.com.pl" . "\r\n";
             mail($to, $subject, $txt, $headers);
         }
     }
