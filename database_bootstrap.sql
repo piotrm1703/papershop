26b275713e6da92769df0990a830aed905400d6c
piotrm1703 <35926833+piotrm1703@users.noreply.github.com>
2018-03-15 15:06:37 +0100

    dodalem podstawowe produkty do bootstrapa sql

--- database_bootstrap.sql
+++ database_bootstrap.sql
@@ -57,6 +57,33 @@ INSERT INTO uploads VALUES
     (NULL, '/uploads/papiersamokopiujacy.jpg'),
     (NULL, '/uploads/papieroffsetowy.jpg');
 
+INSERT INTO users VALUES
+    (NULL,'uzupelnij','uzupelnij','admin','admin','uzupelnij','uzupelnij','uzupelnij','uzupelnij',''),
+    (NULL,'uzupelnij','uzupelnij','administracja','admin1','uzupelnij','uzupelnij','uzupelnij','uzupelnij','');
+
+INSERT INTO products VALUES
+    (NULL,'Papiery-powlekane','Papiery powlekane - produkt 1','2','23.99'),
+    (NULL,'Papiery-powlekane','Papiery powlekane - produkt 2','2','24.99'),
+    (NULL,'Papiery-powlekane','Papiery powlekane - produkt 3','2','25.99'),
+    (NULL,'Papiery-powlekane','Papiery powlekane - produkt 4','2','25.99'),
+    (NULL,'Kartony-graficzne','Kartony graficzne - produkt 1','3','25.99'),
+    (NULL,'Kartony-graficzne','Kartony graficzne - produkt 2','3','26.99'),
+    (NULL,'Kartony-graficzne','Kartony graficzne - produkt 3','3','27.99'),
+    (NULL,'Kartony-opakowaniowe','Kartony opakowaniowe - produkt 1','4','28.99'),
+    (NULL,'Kartony-opakowaniowe','Kartony opakowaniowe - produkt 2','4','29.99'),
+    (NULL,'Kartony-opakowaniowe','Kartony opakowaniowe - produkt 3','4','30.99'),
+    (NULL,'Kartony-opakowaniowe','Kartony opakowaniowe - produkt 4','4','31.99'),
+    (NULL,'Kartony-opakowaniowe','Kartony opakowaniowe - produkt 5','4','32.99'),
+    (NULL,'Papiery-etykietowe','Papier etykietowy - produkt 1','5','33.99'),
+    (NULL,'Papiery-etykietowe','Papier etykietowy - produkt 2','5','34.99'),
+    (NULL,'Papiery-etykietowe','Papier etykietowy - produkt 3','5','35.99'),
+    (NULL,'Papiery-samokopiujace','Papier samokopiujący - produkt 1','6','36.99'),
+    (NULL,'Papiery-samokopiujace','Papier samokopiujący - produkt 2','6','37.99'),
+    (NULL,'Papiery-samokopiujace','Papier samokopiujący - produkt 3','6','38.99'),
+    (NULL,'Papiery-samokopiujace','Papier samokopiujący - produkt 4','6','39.99'),
+    (NULL,'Papier-offsetowy','Papier offsetowy - produkt 1','7','39.99'),
+    (NULL,'Papier-offsetowy','Papier offsetowy - produkt 2','7','39.99');
+
 ALTER DATABASE PaperShop CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 ALTER TABLE users CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
